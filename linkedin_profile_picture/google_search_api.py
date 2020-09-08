1d3aa3f4eff79deb1a2bdf5d535b7556cfc4a9cd
Shashank Deshpande <shashankdeshpande18@gmail.com>
2020-09-08 17:13:04 +0530

    return object

--- linkedin_profile_picture/google_search_api.py
+++ linkedin_profile_picture/google_search_api.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-class GoogleSearchAPI(object):
+class GoogleSearchAPI:
 
     def __init__(self, key: str, cx: str):
         self._cx = cx
@@ -16,14 +16,37 @@ class GoogleSearchAPI(object):
             }
 
     def _hit_api(self, params):
-        api_response = {}
+        api_response = APIResponse()
         try:
             params = dict(params, **self._params)
             params["cx"] = self._cx
             params["key"] = self._key
             resp = requests.get(self._api_url, params=params)
-            if resp and resp.status_code == 200 and "json" in dir(resp):
-                api_response = resp.json()
+            api_response = self._create_api_response(resp)
         except Exception as e:
             logger.info(f"Error in _hit_api: {e}", exc_info=True)
         return api_response
+
+    def _create_api_response(self, resp):
+        link = ""
+        results = []
+        error = None
+        status_code = resp.status_code
+        if status_code == 200:
+            results = resp.json()
+            results = results.get("items",[])
+        else:
+            error = resp.json()
+        return APIResponse(results, status_code, link, error)
+
+class APIResponse:
+
+    def __init__(self,
+            _search_results=[],
+            status_code=400,
+            link="",
+            error=None):
+        self._search_results = _search_results
+        self.status_code = status_code
+        self.link = link
+        self.error = error
--- linkedin_profile_picture/main.py
+++ linkedin_profile_picture/main.py
@@ -14,14 +14,10 @@ class ProfilePicture(object):
                 Input  : linkedin.com/in/shashank-deshpande/
                 Output : shashank-deshpande
         """
-        match = self._check_linkedin_id(link)
+        match = re.findall(r'in\/([^\/]+)\/?', link)
         linkedin_id = match[0].strip() if match else link
         linkedin_id = linkedin_id.strip("/")
         return linkedin_id
-
-    def _check_linkedin_id(self, link: str):
-        match = re.findall(r'in\/([^\/]+)\/?', link)
-        return match
 
     def _check_picture_url(self, link: str) -> bool:
         match = re.findall(r"(media-exp\d\.licdn\.com).+?(profile-displayphoto-shrink_)", link)
@@ -37,29 +33,19 @@ class ProfilePicture(object):
             pass
         return flag
 
-    def _extract_linkedin_results(self, res: list) -> dict:
-        img_dict = {}
+    def _extract_profile_picture(self, linkedin_id: str, res: list) -> str:
+        link = ""
         for i in res:
             pic_url = i.get("link","")
             context_url = i.get("image",{}).get("contextLink","")
-            linkedin_id = self.clean_id(context_url)
+            res_lid = self.clean_id(context_url)
+            if linkedin_id == res_lid and self._check_picture_url(pic_url) and self._check_url_exists(pic_url):
+                link = pic_url
+                break
+        return link
 
-            if self._check_linkedin_id(context_url) and \
-                    self._check_picture_url(pic_url) and \
-                    self._check_url_exists(pic_url) and \
-                    linkedin_id not in img_dict:
-                img_dict[linkedin_id] = pic_url
-
-        return img_dict
-
-    def search(self, params: dict) -> dict:
-        res = self._api_obj._hit_api(params)
-        res = res.get("items",[])
-        res = self._extract_linkedin_results(res)
-        return res
-
-if __name__ == "__main__":
-    obj = ProfilePicture("AIzaSyDYxgxCD3EnmhZI_OyiQOJHv3BJbLnh8zE","6c4e2424c087c5f38")
-    import pdb;pdb.set_trace()
-    qq = obj.search({"q":"matt-eagleson-3871b380"})
-    print(qq)
+    def search(self, params: dict) -> object:
+        linkedin_id = self.clean_id(params.get("q",""))
+        api_resp = self._api_obj._hit_api(params)
+        api_resp.link = self._extract_profile_picture(linkedin_id, api_resp._search_results)
+        return api_resp
