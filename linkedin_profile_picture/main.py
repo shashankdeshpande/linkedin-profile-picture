95733266bf220db323acf838453b646fd67b9768
Shashank Deshpande <shashankdeshpande18@gmail.com>
2020-09-08 14:36:32 +0530

    check valid url

--- linkedin_profile_picture/main.py
+++ linkedin_profile_picture/main.py
@@ -1,4 +1,5 @@
 import re
+import requests
 from .google_search_api import GoogleSearchAPI
 
 class ProfilePicture(object):
@@ -26,14 +27,30 @@ class ProfilePicture(object):
         match = re.findall(r"(media-exp\d\.licdn\.com).+?(profile-displayphoto-shrink_)", link)
         return bool(match)
 
+    def _check_url_exists(self, link):
+        flag = False
+        try:
+            resp = requests.get(link, timeout=5)
+            if resp and resp.status_code == 200:
+                flag = True
+        except:
+            pass
+        return flag
+
     def _extract_linkedin_results(self, res: list) -> dict:
         img_dict = {}
         for i in res:
-            context_url = i.get("image",{}).get("contextLink","")
             pic_url = i.get("link","")
+            thumbnail = i.get("image",{}).get("thumbnailLink","")
+            context_url = i.get("image",{}).get("contextLink","")
+
             if self._check_linkedin_id(context_url) and self._check_picture_url(pic_url):
-                linkedin_id = self.clean_id(context_url)
-                img_dict[linkedin_id] = pic_url
+                priority = filter(None, [pic_url, thumbnail])
+                for url in priority:
+                    if self._check_url_exists(url):
+                        linkedin_id = self.clean_id(context_url)
+                        img_dict[linkedin_id] = pic_url
+                        break
         return img_dict
 
     def search(self, params: dict) -> dict:
